d7e7eb6c6c2ccae1ad411daabf66591b6f286108
ZqinKing <zqinking23@gmail.com>
2026-03-04 15:54:03 +0800

    fix: small-package DMCA #139

--- wrt_core/modules/feeds.sh
+++ wrt_core/modules/feeds.sh
@@ -10,7 +10,7 @@ update_feeds() {
 
     if ! grep -q "small-package" "$FEEDS_PATH"; then
         [ -z "$(tail -c 1 "$FEEDS_PATH")" ] || echo "" >>"$FEEDS_PATH"
-        echo "src-git small8 https://github.com/kenzok8/small-package" >>"$FEEDS_PATH"
+        echo "src-git small8 https://github.com/kenzok8/jell" >>"$FEEDS_PATH"
     fi
 
     if ! grep -q "openwrt-passwall" "$FEEDS_PATH"; then
